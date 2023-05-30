ca8c29ca9edc97150feac89c375f0b326bb88d1e
Abhilash PS <abhilash@jitfin.co>
2023-05-30 15:52:21 +0530

    remove token generator task and make it part of executor script

--- executor.py
+++ executor.py
@@ -1,9 +1,13 @@
 import importlib
 import inspect
 import pkgutil
+import time
 
+import requests
 import tasks
+
 from scripts.tasks.abstract_task import BaseTask
+from scripts.utils.api_calls import SimpleRequests
 
 
 def load_tasks():
@@ -46,7 +50,47 @@ def run_task(task_modules, choice):
     task.execute()
 
 
+def generate_token():
+    base_url = "https://qa.appv2.kyss.ai/apis"
+    simple_requests = SimpleRequests(base_url)
+
+    MAX_ATTEMPTS = 3
+
+    phone_number = input("\nEnter your mobile number: ")
+    otp_endpoint = "/accounts/signin/otp"
+    validate_endpoint = "/accounts/signin/otp/validate"
+
+    for attempt in range(MAX_ATTEMPTS):
+        try:
+            simple_requests.post(otp_endpoint, data={"phone_number": phone_number})
+
+            while True:
+                otp = input(
+                    "Enter the OTP received on your mobile (or type 'resend' to generate a new OTP): "
+                )
+                if otp.lower() == 'resend':
+                    break
+                elif otp.isdigit():
+                    response = simple_requests.post(
+                        validate_endpoint, data={"phone_number": phone_number, "otp": otp}
+                    )
+                    token = response.get('data', {}).get('token')
+                    if token:
+                        print("Token generated successfully.")
+                        simple_requests.set_token(token)
+                        return token
+                else:
+                    print("Invalid input. Please enter a numeric OTP or 'resend' to generate a new OTP.")
+        except requests.exceptions.RequestException as e:
+            print(f"\nAttempt {attempt + 1} failed. Error: {e}")
+            time.sleep(1)  # Wait for a second before retrying
+
+    print("\nMaximum attempts exceeded. Exiting the program.")
+    exit(1)
+
+
 def main():
+    generate_token()  # Generate the token before displaying the menu
     task_modules = load_tasks()
     while True:
         display_menu(task_modules)
