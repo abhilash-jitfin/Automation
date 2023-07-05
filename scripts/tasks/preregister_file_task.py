927db5cd2122d84b5cd52321d508cc15f7881c55
abhilash-jitfin <abhilash@jitfin.co>
2023-07-05 14:34:48 +0530

    Update the code for processing from a directory

--- scripts/tasks/preregister_file_task.py
+++ scripts/tasks/preregister_file_task.py
@@ -1,4 +1,7 @@
 import os
+import shutil
+from os import listdir
+from os.path import isfile, join
 from typing import Optional
 
 import pandas as pd
@@ -7,8 +10,9 @@ from halo import Halo
 
 from ..files.excel import ExcelFile
 from ..utils.api_calls import ApiService
-from ..utils.files import create_directory_if_not_exists
+from ..utils.files import create_directory_if_not_exists, is_valid_directory_path, move_file_to_destination_dir
 from ..utils.settings import load_settings
+from ..utils.terminal import get_clean_input
 from .abstract_task import BaseTask
 
 
@@ -24,16 +28,41 @@ class PreRegisterFileProcessingTask(BaseTask):
 
     def get_params(self) -> None:
         """Get parameters for the task from the user."""
-        self.file_path = input("\nEnter the file path: ").strip()
-        print("\n")
+        while True:
+            self.input_path = get_clean_input("\nEnter the input directory path: ")
+            if is_valid_directory_path(self.input_path):
+                print("\n")
+                break
+            print(f"\nInvalid input directory path '{self.input_path}'")
 
     def execute(self) -> None:
         """Execute the task."""
+        only_files = [file for file in listdir(self.input_path) if isfile(join(self.input_path, file))]
+        input_files = [f for f in only_files if os.path.splitext(f)[1].lower() == ".xlsx"]
+
+        self.processed_dir = os.path.join(self.input_path, "Processed")
+        create_directory_if_not_exists(self.processed_dir)
+
+        self.failed_dir = os.path.join(self.input_path, "Failed")
+        create_directory_if_not_exists(self.failed_dir)
+
+        self.result_dir = os.path.join(self.input_path, "Result")
+        create_directory_if_not_exists(self.result_dir)
+
+        for file_name in input_files:
+            file_path = os.path.join(self.input_path, file_name)
+            print(f"\n- Processing {file_path}")
+            self.process_single_file(file_path)
+            print("\n")
+
+    def process_single_file(self, parent_file_path: str):
+        self.file_path = parent_file_path
+
         df, gstin_dups_df, phone_number_dups_df = self.clean_file()
 
         input_file_name = os.path.basename(self.file_path)
         name_without_extension, _ = os.path.splitext(input_file_name)
-        output_dir = os.path.join(os.path.dirname(self.file_path), name_without_extension)
+        output_dir = os.path.join(os.path.dirname(self.file_path), "temp_files", name_without_extension)
         create_directory_if_not_exists(output_dir)
 
         files = [
@@ -50,7 +79,12 @@ class PreRegisterFileProcessingTask(BaseTask):
         if file_id:
             processed_file_id = self.process_file(file_id)
             if processed_file_id:
-                self.download_file(processed_file_id)
+                output_file_path = self.download_file(processed_file_id)
+                if output_file_path:
+                    move_file_to_destination_dir(parent_file_path, self.processed_dir, can_overwrite=True)
+                    move_file_to_destination_dir(output_file_path, self.result_dir, can_overwrite=True)
+                    return
+        move_file_to_destination_dir(self.file_path, self.failed_dir, can_overwrite=True)
 
     def generate_file_name(self, input_file_name: str, descriptor: str) -> str:
         """Generate an output file name based on the input file name and a descriptor."""
@@ -124,8 +158,8 @@ class PreRegisterFileProcessingTask(BaseTask):
                 )
             file_id = None
             try:
-                file_id = int(response.json().get("data", "")[-2:])
-            except ValueError:
+                file_id = int(response.json().get("data", {})["file_id"])
+            except (ValueError, KeyError) as e:
                 print("Failed to upload the file.")
                 return
             if file_id:
@@ -133,6 +167,7 @@ class PreRegisterFileProcessingTask(BaseTask):
                 return file_id
         except requests.exceptions.RequestException as e:
             spinner.fail(f"Failed to upload the file. Error: {e}")
+            print(str(e))
 
         return None
 
@@ -145,14 +180,13 @@ class PreRegisterFileProcessingTask(BaseTask):
             if response.status_code == 200:
                 spinner.succeed("File processed successfully.")
                 return file_id
-            else:
-                print("Failed to process the file.")
+            spinner.fail("Failed to process the file.")
         except requests.exceptions.RequestException as e:
             spinner.fail(f"Failed to process the file. Error: {e}")
 
         return None
 
-    def download_file(self, file_id: str) -> None:
+    def download_file(self, file_id: str) -> Optional[str]:
         """Download the processed file."""
         spinner = Halo(text="Downloading File", spinner="dots")
         spinner.start()
@@ -165,7 +199,7 @@ class PreRegisterFileProcessingTask(BaseTask):
                     for chunk in response.iter_content(chunk_size=1024):
                         file.write(chunk)
                 spinner.succeed(f"Processed file downloaded successfully: {os.path.basename(output_file_path)}")
-            else:
-                spinner.fail("Failed to download the processed file.")
+                return output_file_path
+            spinner.fail("Failed to download the processed file.")
         except requests.exceptions.RequestException as e:
             spinner.fail(f"Failed to download the processed file. Error: {e}")
--- scripts/utils/files.py
+++ scripts/utils/files.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 
 def create_directory_if_not_exists(directory_path):
@@ -21,3 +22,15 @@ def is_valid_directory_path(path):
     if os.path.isfile(path):
         return False
     return False
+
+
+def move_file_to_destination_dir(source_file_path: str, destination_dir: str, can_overwrite: bool = False) -> None:
+    if not os.path.isfile(source_file_path):
+        raise FileNotFoundError(f"The specified path '{source_file_path}' is not a valid file path.")
+    destination_file_path = os.path.join(destination_dir, os.path.basename(source_file_path))
+    if os.path.exists(destination_file_path) and can_overwrite:
+        os.remove(destination_file_path)
+    try:
+        shutil.move(source_file_path, destination_file_path)
+    except shutil.Error as e:
+        print(f"{str(e)}\n")
